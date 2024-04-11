21ddf151f1bf2441c3c09d7f79be393ae11426d3
luiszink <157110320+luiszink@users.noreply.github.com>
2024-04-11 20:53:04 +0200

    Merge-Conflict Test

--- src/main/scala/example/main.scala
+++ src/main/scala/example/main.scala
@@ -2,6 +2,8 @@
 object Main {
   def main(args: Array[String]): Unit = {
     KniffelApp.main(Array.empty) // Aufruf der main-Methode von KniffelApp
-    println("This should cause a Merge-Conflict")
+    println("This should cause a Merge-Conflict from the Branch featur-michi")
+    println("This should cause a Merge-Conflict from the Branch featur-michi")
+
   }
 }
