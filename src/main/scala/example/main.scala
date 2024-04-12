09a75f1a7feb6969f33792092c68354bff7a5d35
ott-michael <164868486+ott-michael@users.noreply.github.com>
2024-04-12 10:18:33 +0200

    / First push from ott-michael

--- src/main/scala/example/main.scala
+++ src/main/scala/example/main.scala
@@ -3,7 +3,7 @@ object Main {
   def main(args: Array[String]): Unit = {
     KniffelApp.main(Array.empty) // Aufruf der main-Methode von KniffelApp
     println("This should cause a Merge-Conflict from the Branch featur-michi")
-    println("This should cause a Merge-Conflict from the Branch featur-michi")
+    // First push from ott-michael
 
   }
 }
