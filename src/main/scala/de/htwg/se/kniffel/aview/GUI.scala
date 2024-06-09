ecba762ee87422343f7b9147d964c426629c07a9
luiszink <157110320+luiszink@users.noreply.github.com>
2024-06-09 15:54:11 +0200

    Changed yahtzee->kniffel

--- src/main/scala/de/htwg/se/kniffel/aview/GUI.scala
+++ src/main/scala/de/htwg/se/kniffel/aview/GUI.scala
@@ -4,33 +4,70 @@ import de.htwg.se.kniffel.controller.Controller
 import de.htwg.se.kniffel.util.Observer
 import scalafx.application.JFXApp3
 import scalafx.scene.Scene
-import scalafx.scene.layout.Pane
-import scalafx.scene.control.TableView
-import scalafx.scene.control.TableColumn
+import scalafx.scene.layout.{Pane, VBox, HBox}
+import scalafx.scene.control.{TableView, TableColumn, Button, Label, CheckBox}
 import scalafx.beans.property.StringProperty
 import scalafx.collections.ObservableBuffer
 import scalafx.application.Platform
-import de.htwg.se.kniffel.model.Player
+import scalafx.stage.Screen
+import scalafx.geometry.{Insets, Pos}
+import scala.compiletime.uninitialized
+import javafx.stage._
+
 
 class GUI(controller: Controller) extends JFXApp3 with Observer {
   controller.add(this)
 
-  var pane: Pane = null;
-  var tableView: TableView[(String, String)] = null;
+  var tableView: TableView[(String, String)] = uninitialized
+  var rollButton: Button = uninitialized
+  var diceResultsLabel: Label = uninitialized
+  var diceCheckBoxes: Seq[CheckBox] = uninitialized
 
   override def start(): Unit = {
-    stage = new JFXApp3.PrimaryStage {
-      title = "Kniffel"
-      resizable = false
-      scene = new Scene(
-        600,
-        600
-      ) {
-        pane = new Pane()
-        tableView = new TableView[(String, String)]()
-        pane.children = Seq(tableView)
-        content = pane
+    try {
+      val screenBounds = Screen.primary.visualBounds
+
+      stage = new JFXApp3.PrimaryStage {
+        title = "Kniffel"
+        resizable = true
+        scene = new Scene(screenBounds.width, screenBounds.height) {
+
+          val pane = new Pane()
+
+          tableView = new TableView[(String, String)]()
+
+          rollButton = new Button("Würfeln")
+          rollButton.onAction = _ => rollDice()
+
+          diceResultsLabel = new Label("Würfelergebnisse: ")
+
+          diceCheckBoxes = (1 to 5).map { i =>
+            new CheckBox(s"Dice $i")
+          }
+
+          val diceBox = new VBox(10) {
+            children = Seq(rollButton, diceResultsLabel) ++ diceCheckBoxes
+            padding = Insets(20)
+            alignment = Pos.Center
+          }
+
+          val hbox = new HBox(20) {
+            children = Seq(tableView, diceBox)
+            padding = Insets(20)
+            alignment = Pos.Center
+          }
+          hbox.layoutX = 50
+          hbox.layoutY <== (pane.height - hbox.height) / 2
+
+          pane.children = hbox
+          content = pane
+        }
+
       }
+
+    } catch {
+      case e: Exception =>
+        e.printStackTrace()
     }
   }
 
@@ -38,13 +75,14 @@ class GUI(controller: Controller) extends JFXApp3 with Observer {
     Platform.runLater(message match {
       case "printScoreCard" => scorecard()
       case "playerAdded"    => scorecard()
-      case _                => println("wrong notifyObservers!!!!!!!!!!!!!!")
+      case "printDice"      => updateDiceResults()
+      case _                => println("")
     })
   }
 
   def scorecard(): Unit = {
-    tableView.columns.clear();
-    val score = controller.getCurrentPlayer.scoreCard;
+    tableView.columns.clear()
+    val score = controller.getCurrentPlayer.scoreCard
     val players = controller.getPlayers
     val categoryColumn =
       new TableColumn[(String, String), String]("Category") {
@@ -72,7 +110,6 @@ class GUI(controller: Controller) extends JFXApp3 with Observer {
       }
     }
 
-// Add columns to the TableView
     tableView.columns ++= List(categoryColumn)
     tableView.columns ++= playerColumns
 
@@ -85,7 +122,23 @@ class GUI(controller: Controller) extends JFXApp3 with Observer {
     }: _*)
 
     tableView.items = scoreCardEntries
-
   }
 
+  def rollDice(): Unit = {
+    val keptDiceIndices = diceCheckBoxes.zipWithIndex.collect {
+      case (checkBox, index) if checkBox.selected.value => index + 1
+    }.toList
+    controller.keepDice(keptDiceIndices)
+    if (controller.repetitions == 0) {
+      rollButton.disable = true
+    }
+  }
+
+  def updateDiceResults(): Unit = {
+    val diceResults = controller.getDice
+    diceResultsLabel.text = s"Würfelergebnisse: ${diceResults.mkString(", ")}"
+    diceCheckBoxes.zip(diceResults).foreach { case (checkBox, result) =>
+      checkBox.text = s"Dice: $result"
+    }
+  }
 }
