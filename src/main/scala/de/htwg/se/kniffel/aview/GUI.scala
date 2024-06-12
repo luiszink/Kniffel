0911b9bbd9053b0fb8882795ebb441d016bdc9bd
ott-michael <164868486+ott-michael@users.noreply.github.com>
2024-06-12 23:15:34 +0200

    new screen with adding names

--- src/main/scala/de/htwg/se/kniffel/aview/GUI.scala
+++ src/main/scala/de/htwg/se/kniffel/aview/GUI.scala
@@ -14,7 +14,9 @@ import scalafx.stage.Screen
 import scalafx.geometry.{Insets, Pos}
 import scalafx.scene.image.{Image, ImageView}
 import scala.compiletime.uninitialized
+import scalafx.Includes.jfxRectangle2D2sfx
 import javafx.stage._
+import scalafx.scene.control.TextField
 
 class GUI(controller: Controller) extends JFXApp3 with Observer {
   controller.add(this)
@@ -26,6 +28,7 @@ class GUI(controller: Controller) extends JFXApp3 with Observer {
   var diceCheckBoxes: Seq[CheckBox] = uninitialized
   var updateCategoryButton: Button = uninitialized
   var selectedCategory: String = ""
+  var playerNameFields: Seq[TextField] = uninitialized
 
   override def update(event: KniffelEvent.Value): Unit = {
     Platform.runLater(event match {
@@ -43,59 +46,88 @@ class GUI(controller: Controller) extends JFXApp3 with Observer {
       stage = new JFXApp3.PrimaryStage {
         title = "Kniffel"
         resizable = true
-        scene = new Scene(screenBounds.width, screenBounds.height) {
-
-          val pane = new Pane()
-
-          tableView = new TableView[(String, String)]()
-          tableView.onMouseClicked = _ => handleCategorySelection()
-
-          rollButton = new Button("Roll Dice")
-          rollButton.onAction = _ => rollDice()
-
-          updateCategoryButton = new Button("Update Category")
-          updateCategoryButton.onAction = _ => updateSelectedCategory()
-
-          diceResultsLabel = new Label("Dice Results: ")
-
-          diceImageViews = (1 to 5).map { i =>
-            new ImageView {
-              fitHeight = 50
-              fitWidth = 50
-            }
-          }
-
-          diceCheckBoxes = (1 to 5).map { i =>
-            new CheckBox(s"Keep Dice $i")
-          }
-
-          val diceBox = new VBox(10) {
-            children = Seq(rollButton, updateCategoryButton, diceResultsLabel) ++
-              diceImageViews.zip(diceCheckBoxes).flatMap { case (imageView, checkBox) =>
-                Seq(imageView, checkBox)
-              }
-            padding = Insets(20)
-            alignment = Pos.Center
-          }
-
-          val hbox = new HBox(20) {
-            children = Seq(tableView, diceBox)
-            padding = Insets(20)
-            alignment = Pos.Center
-          }
-          hbox.layoutX = 50
-          hbox.layoutY <== (pane.height - hbox.height) / 2
-
-          pane.children = hbox
-          content = pane
-        }
-
+        scene = playerNameScene(screenBounds)
       }
 
     } catch {
       case e: Exception =>
         e.printStackTrace()
     }
+  }
+
+  def playerNameScene(screenBounds: javafx.geometry.Rectangle2D): Scene = {
+    new Scene(screenBounds.width, screenBounds.height) {
+      val pane = new Pane()
+      playerNameFields = (1 to 4).map { i => new TextField { promptText = s"Player $i Name" } }
+
+      val confirmButton = new Button("Confirm")
+      confirmButton.onAction = _ => handlePlayerNames()
+
+      val vbox = new VBox(10) {
+        children = playerNameFields :+ confirmButton
+        padding = Insets(20)
+        alignment = Pos.Center
+      }
+
+      pane.children = vbox
+      content = pane
+    }
+  }
+
+  def mainGameScene(screenBounds: javafx.geometry.Rectangle2D): Scene = {
+    new Scene(screenBounds.width, screenBounds.height) {
+
+      val pane = new Pane()
+
+      tableView = new TableView[(String, String)]()
+      tableView.onMouseClicked = _ => handleCategorySelection()
+
+      rollButton = new Button("Roll Dice")
+      rollButton.onAction = _ => rollDice()
+
+      updateCategoryButton = new Button("Update Category")
+      updateCategoryButton.onAction = _ => updateSelectedCategory()
+
+      diceResultsLabel = new Label("Dice Results: ")
+
+      diceImageViews = (1 to 5).map { i =>
+        new ImageView {
+          fitHeight = 50
+          fitWidth = 50
+        }
+      }
+
+      diceCheckBoxes = (1 to 5).map { i =>
+        new CheckBox(s"Keep Dice $i")
+      }
+
+      val diceBox = new VBox(10) {
+        children = Seq(rollButton, updateCategoryButton, diceResultsLabel) ++
+          diceImageViews.zip(diceCheckBoxes).flatMap { case (imageView, checkBox) =>
+            Seq(imageView, checkBox)
+          }
+        padding = Insets(20)
+        alignment = Pos.Center
+      }
+
+      val hbox = new HBox(20) {
+        children = Seq(tableView, diceBox)
+        padding = Insets(20)
+        alignment = Pos.Center
+      }
+      hbox.layoutX = 50
+      hbox.layoutY <== (pane.height - hbox.height) / 2
+
+      pane.children = hbox
+      content = pane
+    }
+  }
+
+  def handlePlayerNames(): Unit = {
+    val names = playerNameFields.map(_.text.value.trim).filter(_.nonEmpty)
+    names.foreach(controller.addPlayer)
+    val screenBounds = Screen.primary.visualBounds
+    stage.scene = mainGameScene(screenBounds)
   }
 
   def handleCategorySelection(): Unit = {
